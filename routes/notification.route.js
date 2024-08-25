a5f1b276a4111f2314fd04a297bb4861949533ae
rajZadafiya55 <zadagfiyaraj395.com>
2024-08-25 13:26:46 +0530

    first commit

---
+++ routes/notification.route.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { deleteNotifications, deleteSingleNotification, getNotifications } from "../controllers/notification.controller.js";
+
+const router = express.Router();
+
+router.get("/", protectRoute, getNotifications);
+router.delete("/", protectRoute, deleteNotifications);
+router.delete("/:id", protectRoute, deleteSingleNotification);
+
+export default router;
